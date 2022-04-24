ab959ec2454aa89d1901bcd38ed84148148d49b9
knighd <daizhenjie@xiaoyouzi.com>
2022-04-24 14:45:46 +0800

    feat: pure demo 1

--- src/modules/a.js
+++ src/modules/a.js
@@ -9,3 +9,10 @@ export const foo1 = () => {
 export const foo2 = () => {
   console.log('foo2')
 }
+
+const init_impure_foo = () => {
+  window.impure_foo = 'impure_foo'
+  return 'impure_foo'
+}
+
+export const impure_foo = init_impure_foo()
